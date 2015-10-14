629d415773552b827e9a54613bbc305a71fef47f
Mike Watson <mwatson.ca@gmail.com>
2015-10-14 16:39:36 -0400

    Setting the basic frame for the GUI

--- NHLgui.py
+++ NHLgui.py
@@ -1,1 +1,14 @@
 # This will be the NHLgui application
+
+# importing the relevent modules
+import Tkinter as *
+
+# creates the window
+root = Tk()
+
+# modify the root window
+root.title("NHL GUI Application")
+root.geometry("500x500")
+
+# launch the event loop
+root.mainloop()
